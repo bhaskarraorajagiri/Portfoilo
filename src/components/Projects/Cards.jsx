4bb9404a45a31b37e35d2e071922e2a62ca3bdf1
bhaskarrajagiri <bhaskarraorajagiri@gmail.com>
2025-07-27 16:51:48 +0530

    projects updated

--- src/components/Projects/Cards.jsx
+++ src/components/Projects/Cards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Cards({src,link,title,disc}) {
   return (
     <a href={link} target='_blank'>
-                <img className='hover' src={src} alt="BlackJack-logo" />
+                <img className='hover' src={src} alt="project-logo" />
                 <h3>{title}</h3>
                 <p>{disc}</p>
             </a>
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,30 +1,77 @@
 import style from './Project.module.css';
-import BlackJack from '../../assets/blackjack.jpg'
-import Calculator from '../../assets/calculator.jpg'
-import Extension from '../../assets/extension.jpg'
+import BlackJack from '../../assets/blackjack-logo.png';
+import Calculator from '../../assets/calculator.jpg';
+import Extension from '../../assets/chrome-extension-logo.png';
+import portfolio from '../../assets/portfolio-logo.png';
+import steganogerphy from '../../assets/steganography-logo.png';
+import univesity from '../../assets/university-management-system-logo.png';
+import landingpage from '../../assets/landingpage-logo.png';
 import Cards from './Cards';
-
 
 function Project() {
   return (
     <section id='projects' className={style.Container}>
-        <h1 className={style.sectionTitle}>Projects</h1>
-        <div className={style.ProjectContainer}>
-            <Cards src={BlackJack} 
+      <h1 className={style.sectionTitle}>Projects</h1>
+      <div className={style.ProjectContainer}>
+        <div className={style.cardWrapper}>
+          <Cards
+            src={portfolio}
+            link="https://github.com/bhaskarraorajagiri/Portfoilo"
+            title="Personal Portfolio"
+            disc="Web development showcase"
+          />
+        </div>
+        <div className={style.cardWrapper}>
+          <Cards
+            src={BlackJack}
             link="https://github.com/bhaskarraorajagiri/Black-Jack-Game"
             title="BlackJack"
-            disc="Gaming Platform"/>
-            <Cards src={Calculator} 
+            disc="Playful, crisp, gaming."
+          />
+        </div>
+        <div className={style.cardWrapper}>
+          <Cards
+            src={steganogerphy}
+            link="https://github.com/bhaskarraorajagiri/Stenography"
+            title="Steganography"
+            disc="Hidden, secure, digital insight."
+          />
+        </div>
+        <div className={style.cardWrapper}>
+          <Cards
+            src={univesity}
+            link="https://github.com/bhaskarraorajagiri/University-Management-System"
+            title="University Management System"
+            disc="Academic, organized, efficient management."
+          />
+        </div>
+        <div className={style.cardWrapper}>
+          <Cards
+            src={landingpage}
+            link="https://github.com/bhaskarraorajagiri/LandingPage"
+            title="Landing Page"
+            disc="Modern, vibrant, growth-oriented."
+          />
+        </div>
+        <div className={style.cardWrapper}>
+          <Cards
+            src={Calculator}
             link="https://github.com/bhaskarraorajagiri/CodSoft/tree/main/calculator"
             title="Calculator"
-            disc="App"/>
-            <Cards src={Extension} 
+            disc="Precise, functional, utility tool."
+          />
+        </div>
+        <div className={style.cardWrapper}>
+          <Cards
+            src={Extension}
             link="https://github.com/bhaskarraorajagiri/Chrome-Extenstion"
             title="Chrome-Extension"
-            disc="simple extension"/>
+            disc="Functional, sleek, browser utility."
+          />
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Project
+export default Project;
