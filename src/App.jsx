9b6dd64a1c820d07ba141b08d1eac74223808edd
bhaskarrajagiri <bhaskarraorajagiri@gmail.com>
2025-07-28 12:00:51 +0530

    corrected NavBar import casing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import './App.css'
 import Hero from './components/Hero'
-import Navbar from './components/NavBar/NavBar'
+import NavBar from './components/NavBar/NavBar' 
 import About from './components/AboutMe'
 import Project from './components/Projects/Project'
 import Skill from './components/skills/Skill'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+
 function App() {
   return (
     <>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Project/>
-      <Skill/>
-      <Contact/>
-      <Footer/>
+      <NavBar /> 
+      <Hero />
+      <About />
+      <Project />
+      <Skill />
+      <Contact />
+      <Footer />
     </>
   )
 }
